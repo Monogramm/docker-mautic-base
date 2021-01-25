f37c72d8f35225ef2a970fc94831b9d9a5feb7be
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-01-25 14:01:36 +0100

    ⬆️ Upgrade Mautic and add hooks
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- update.sh
+++ update.sh
@@ -24,19 +24,23 @@ variants=(
 	fpm
 )
 
-echo "get current version"
-current=$( curl -fsSL 'https://api.github.com/repos/mautic/mautic/tags' |tac|tac| \
+echo "get latest version"
+latest=$( curl -fsSL 'https://api.github.com/repos/mautic/mautic/tags' |tac|tac| \
 	grep -oE '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | \
 	sort -urV | \
 	head -n 1 )
+version=$(echo "$latest" | cut -d. -f1-2)
+major=$(echo "$latest" | cut -d. -f1-1)
+
+dockerLatest='3.2'
 
 # TODO - Expose SHA signatures for the packages somewhere
-echo "get $current SHA signature"
-curl -o mautic.zip -SL "https://github.com/mautic/mautic/releases/download/$current/$current.zip"
+echo "get $latest SHA signature"
+curl -o mautic.zip -SL "https://github.com/mautic/mautic/releases/download/$latest/$latest.zip"
 sha1="$(sha1sum mautic.zip | sed -r 's/ .*//')"
 
-echo "get $current src SHA signature"
-curl -o mautic-src.zip -SL "https://github.com/mautic/mautic/archive/$current.zip"
+echo "get $latest src SHA signature"
+curl -o mautic-src.zip -SL "https://github.com/mautic/mautic/archive/$latest.zip"
 srcsha1="$(sha1sum mautic-src.zip | sed -r 's/ .*//')"
 
 echo "remove mautic-src.zip"
@@ -46,23 +50,40 @@ echo "update docker images"
 travisEnv=
 for variant in "${variants[@]}"; do
 	dir="$variant"
-	echo "generating $current-$variant"
+	echo "generating $latest-$variant"
 
 	template="Dockerfile-${base[$variant]}.template"
 	cp $template "$dir/Dockerfile"
 
+	# To make management easier, we use these files for all variants
+	cp -r common/* "$dir/"
+
 	sed -E -i'' -e '
 		s/%%VARIANT%%/'"$variant"'/;
 		s/%%VARIANT_EXTRAS%%/'"${extras[$variant]}"'/;
-		s/%%VERSION%%/'"$current"'/;
+		s/%%VERSION%%/'"$latest"'/;
 		s/%%VERSION_SHA1%%/'"$sha1"'/;
 		s/%%VERSION_SRC_SHA1%%/'"$srcsha1"'/;
 		s/%%CMD%%/'"${cmd[$variant]}"'/;
 	' "$dir/Dockerfile"
 
-	# To make management easier, we use these files for all variants
-	cp common/* "$dir"/
+	# Create a list of "alias" tags for DockerHub post_push
+	if [ "$version" = "$dockerLatest" ]; then
+		if [ "$variant" = 'apache' ]; then
+			export DOCKER_TAGS="$latest-$variant $version-$variant $major-$variant $variant $latest $version $major latest "
+		else
+			export DOCKER_TAGS="$latest-$variant $version-$variant $major-$variant $variant "
+		fi
+	else
+		if [ "$variant" = 'apache' ]; then
+			export DOCKER_TAGS="$latest-$variant $version-$variant $major-$variant $latest $version "
+		else
+			export DOCKER_TAGS="$latest-$variant $version-$variant $major-$variant "
+		fi
+	fi
+	echo "${DOCKER_TAGS}" > "$dir/.dockertags"
 
+	# Add Travis-CI env var
 	travisEnv='\n    - VARIANT='"$variant$travisEnv"
 done
 
