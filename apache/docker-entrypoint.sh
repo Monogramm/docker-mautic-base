d9677aa165f94a83a3678ee154c7ed2a99c94a44
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-01-26 23:42:42 +0100

    :art: Improve entrypoint format
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -85,7 +85,7 @@ if [ -n "$MAUTIC_CRON_DYNAMICS" ]; then
         echo "10,40 * * * *     www-data   php /var/www/html/app/console mautic:integration:fetchleads -i Dynamics > /var/log/cron.pipe 2>&1" >> /etc/cron.d/mautic
 fi
 
-if ! [ -e index.php -a -e app/AppKernel.php ]; then
+if ! [ -e index.php ] && [ -e app/AppKernel.php ]; then
         echo >&2 "Mautic not found in $(pwd) - copying now..."
 
         if [ "$(ls -A)" ]; then
@@ -96,8 +96,8 @@ if ! [ -e index.php -a -e app/AppKernel.php ]; then
         tar cf - --one-file-system -C /usr/src/mautic . | tar xf -
 
         echo >&2 "Copying Mautic composer config now..."
-        cp /usr/src/mautic-src/mautic-$MAUTIC_VERSION/composer.json .
-        cp /usr/src/mautic-src/mautic-$MAUTIC_VERSION/composer.lock .
+        cp "/usr/src/mautic-src/mautic-$MAUTIC_VERSION/composer.json" .
+        cp "/usr/src/mautic-src/mautic-$MAUTIC_VERSION/composer.lock" .
 
         echo >&2 "Complete! Mautic has been successfully copied to $(pwd)"
 fi
@@ -108,7 +108,7 @@ if [ -n "$MAUTIC_PLUGINS" ]; then
 
         for plugin in $MAUTIC_PLUGINS; do
                 echo "Adding requirements for $plugin..."
-                composer require $plugin \
+                composer require "$plugin" \
                         --prefer-dist --prefer-stable --no-update \
                         --no-interaction --optimize-autoloader;
         done
